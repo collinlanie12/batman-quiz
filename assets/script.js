2ce3a3f2e0a1026716e8f824dcc7554f1cc01c38
Collin Lanier <collinlanier12@gmail.com>
2020-08-06 18:59:01 -0400

    added questions for quiz in js, index with css. Created start page for quiz.

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+var batQuestions = [
+    {
+        question: "What is Batman's secret identity?",
+        answerChoicesArr: ["Bruce Wayne", "Damian Wayne", "Clark Kent", "James Gordon"],
+        answerID: 1
+    },
+    {
+        question: "What is Batman's biological son?",
+        answerChoicesArr: ["Dick", "Jason", "Damien", "Tim"],
+        answerID: 3
+    },
+    {
+        question: "Which of the robins died and came back to life?",
+        answerChoicesArr: ["Tim Drake", "Dick Grayson", "Damian Wayne", "Jason Todd"],
+        answerID: 4
+    },
+    {
+        question: "What is the name of Batgirl?",
+        answerChoicesArr: ["Kathy Kane", "Barbara Gordon", "Selina Kyle", "Harley Quinn"],
+        answerID: 2
+    },
+    {
+        question: "What villian mainly antagonized Dick Grayson when he was the leader of the teen Titans?",
+        answerChoicesArr: ["Bane", "Joker", "Deathstroke", "Deadshot"],
+        answerID: 3
+    }
+]
